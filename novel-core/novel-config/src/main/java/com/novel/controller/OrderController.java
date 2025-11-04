395910dc511a2ca58321c06d7ff9818d2b3b6efd
Cheunry <cheunry@CheunrydeMacBook-Pro.local>
2025-11-04 13:54:05 +0800

    小饼的超级大屌教程

--- novel-core/novel-config/src/main/java/com/novel/controller/OrderController.java
+++ novel-core/novel-config/src/main/java/com/novel/controller/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrderController {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("I love small pie's big dick");
     }
 }
 
